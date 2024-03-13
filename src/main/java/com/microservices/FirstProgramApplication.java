570a138e3e08aa4f04aba6a34df549e266f52606
mihussain737 <mihussain737@gmail.com>
2024-03-13 10:33:10 +0530

    add created by

--- src/main/java/com/microservices/FirstProgramApplication.java
+++ src/main/java/com/microservices/FirstProgramApplication.java
@@ -2,8 +2,10 @@ package com.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class FirstProgramApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/microservices/service/impl/AccountsServiceImpl.java
+++ src/main/java/com/microservices/service/impl/AccountsServiceImpl.java
@@ -38,8 +38,6 @@ public class AccountsServiceImpl implements IAccountsService {
             throw new CustomerAlreadyExistsException("Customer already exist with given mobileNumber"
             +customerDto.getMobileNumber());
         }
-        customer.setCreatedAt(LocalDateTime.now());
-        customer.setCreatedBy("Anonymous");
         Customer savedCustomer=customerRepository.save(customer);
         accountsRepository.save(createNewAccount(savedCustomer));
     }
@@ -56,8 +54,6 @@ public class AccountsServiceImpl implements IAccountsService {
         newAccount.setAccountNumber(randomAccNumber);
         newAccount.setAccountType(AccountsConstants.SAVINGS);
         newAccount.setBranchAddress(AccountsConstants.ADDRESS);
-        newAccount.setCreatedAt(LocalDateTime.now());
-        newAccount.setCreatedBy("anonymous");
         return newAccount;
     }
 
